0848aba39c1ea273cf6bb13f4fa4d8e340451353
Bruce Pascoe <fatcerberus@icloud.com>
2023-12-14 19:58:21 -0500

    Implement configuration
    No error handling yet, but it seems to work.  Not sure what I'll work on
    next; probably command-line parsing.

--- src/main.js
+++ src/main.js
@@ -1,50 +1,63 @@
+import { readFile } from 'node:fs/promises';
+import path, { dirname } from 'node:path';
+import { fileURLToPath } from 'node:url';
+
 import { Server } from './server.js';
 
 const APP_NAME = "Hetap";
 const APP_COPYRIGHT = "2024 Fat Cerberus";
-const APP_VERSION = "0.0.0+";
+const APP_VERSION = "0.1.0-dev";
 
-console.log(`${APP_NAME} ${APP_VERSION} easy-to-use Web server`);
-console.log(`a quick-and-dirty file system-based Web server`);
-console.log(`(c) ${APP_COPYRIGHT}`);
+printVersion();
+
+const config = await loadConfig();
+const options = parseCommandLine();
+
+const hostname = "localhost";
+const port = 8080;
+console.log(`starting HTTP server...`);
+console.log(`   hostname: ${hostname ?? "<any>"}`);
+console.log(`   port: ${port}`);
+const server = new Server(config.baseConfig)
+	.mount('/', config.rootPath);
+for (const [ mount, hostPath ] of Object.entries(options.mounts)) {
+	const prefix = config.mountPoints[mount];
+	server.mount(prefix, path.resolve(hostPath));
+}
+await server.start(port, hostname);
+
 console.log();
 
-const serverConfig = {
-	indexFileNames: [
-		"index.html",
-		"index.htm"
-	],
-	mimeTypes: {
-		".bmp":  "image/bmp",
-		".css":  "text/css",
-		".csv":  "text/csv",
-		".gif":  "image/gif",
-		".htm":  "text/html",
-		".html": "text/html",
-		".ico":  "image/vnd.microsoft.icon",
-		".jar":  "application/java-archive",
-		".jpeg": "image/jpeg",
-		".jpg":  "image/jpeg",
-		".js":   "text/javascript",
-		".json": "application/json",
-		".mjs":  "text/javascript",
-		".mp3":  "audio/mpeg",
-		".ogg":  "audio/ogg",
-		".ogv":  "video/ogg",
-		".opus": "audio/opus",
-		".pdf":  "application/pdf",
-		".png":  "image/png",
-		".svg":  "image/svg+xml",
-		".ttf":  "font/ttf",
-		".txt":  "text/plain",
-		".wav":  "audio/wav",
-		".webp": "image/webp",
-		".xml":  "application/xml",
-		".zip":  "application/zip"
-	}
-};
+async function loadConfig()
+{
+	console.log(`reading configuration files...`);
+	const userConfig = JSON.parse(await readFile('./hetap.json'));
+	const configPath = path.resolve(dirname(fileURLToPath(import.meta.url)), `../configs/${userConfig.uses}.json`);
+	const baseConfig = JSON.parse(await readFile(configPath));
+	return {
+		baseConfig,
+		rootPath: path.resolve(userConfig.root),
+		mountPoints: userConfig.mountPoints
+	};
+}
 
-await new Server(serverConfig)
-	.mount("/", ".\\")
-	.mount("/dist", "C:\\src\\spectacles\\dist")
-	.start(8080, "localhost");
+function parseCommandLine()
+{
+	// TODO: ACTUALLY parse the command line
+	return {
+		mounts: {
+			"game": "C:\\src\\spectacles\\dist"
+		}
+	};
+}
+
+function printVersion(showDeps = false)
+{
+	console.log(`${APP_NAME} ${APP_VERSION} file-based Web server`);
+	console.log(`a lightweight Web server hosted from the file system`);
+	console.log(`(c) ${APP_COPYRIGHT}`);
+	console.log();
+	if (showDeps) {
+		console.log(`   Node.js: ${process.version}`);
+	}
+}
--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
-import * as FS from 'node:fs/promises';
-import HTTP from 'node:http';
-import Path from 'node:path';
+import { readFile, stat } from 'node:fs/promises';
+import http from 'node:http';
+import path from 'node:path';
 
 export
 class Server
@@ -17,15 +17,15 @@ class Server
 
 	mount(prefix, hostPath)
 	{
-		this.#endpoints[prefix] = Path.resolve(hostPath);
+		this.#endpoints[prefix] = path.resolve(hostPath);
 		return this;
 	}
 
-	async start(port = 80, hostname = null)
+	async start(port = 80, hostname)
 	{
-		return await new Promise((resolve, reject) => {
-			HTTP.createServer(this.#serveRequest.bind(this))
-				.listen(port, hostname ?? undefined, () => resolve())
+		return new Promise((resolve, reject) => {
+			http.createServer(this.#serveRequest.bind(this))
+				.listen(port, hostname, () => resolve())
 		});
 	}
 
@@ -35,15 +35,15 @@ class Server
 			.filter(([ prefix, hostPath ]) => request.url.startsWith(prefix))
 			.map(([ prefix, hostPath ]) => `${hostPath}/${request.url.slice(prefix.length)}`)
 			.at(-1);
-		let filePath = Path.resolve(localPath);
-		let content = null;
+		let filePath = path.resolve(localPath);
+		let fileContent = null;
 		try {
-			const stats = await FS.stat(filePath);
+			const stats = await stat(filePath);
 			if (stats.isDirectory()) {
 				for (const indexFileName of this.#indexFileNames) {
 					try {
-						filePath = Path.resolve(localPath, indexFileName);
-						content = await FS.readFile(filePath);
+						filePath = path.resolve(localPath, indexFileName);
+						fileContent = await readFile(filePath);
 						break;
 					}
 					catch {
@@ -52,21 +52,21 @@ class Server
 				}
 			}
 			else {
-				content = await FS.readFile(filePath);
+				fileContent = await readFile(filePath);
 			}
 		}
 		catch {
 			/* *munch* */
 		}
-		if (content !== null) {
-			const fileExtension = Path.extname(filePath);
+		if (fileContent !== null) {
+			const fileExtension = path.extname(filePath);
 			const mimeType = this.#mimeTypes[fileExtension] ?? 'application/octet-stream';
 			console.log(`200 - '${request.url}' - ${mimeType} - ${filePath}`);
 			response.writeHead(200, { 'Content-Type': mimeType });
-			response.end(content);
+			response.end(fileContent);
 		}
 		else {
-			console.log(`\x1B[31;1m404 - '${request.url}' - no suitable file could be found\x1B[m`);
+			console.log(`\x1B[31;1m404 - '${request.url}' - no suitable file available\x1B[m`);
 			response.writeHead(404);
 			response.end("404: File not found");
 		}
